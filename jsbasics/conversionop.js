262b1a4f295a60b6f18de2688b70c094aa98796b
M Saad Asad <saadasadcs@gmail.com>
2024-07-21 11:01:58 +0000

    conversion and operations

--- jsbasics/conversionop.js
+++ jsbasics/conversionop.js
@@ -24,3 +24,41 @@ let valueInNumber = Number(name)
 console.log(valueInNumber)
 
 console.log(typeof valueInNumber)
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " brother"
+
+let str3 = str1 + str2 // concatenation
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); from left to right 
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // not a good practice
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
