efbde8307a5631b03a140c202fd4a9d3f8dbc642
M Saad Asad <saadasadcs@gmail.com>
2024-07-21 10:35:44 +0000

    conversion

---
+++ jsbasics/conversionop.js
@@ -0,0 +1,26 @@
+let sapId = 70109951
+
+console.log(sapId)
+
+console.log(typeof (sapId))
+
+// conversion number to string 
+
+let stringsapId = String(sapId)
+
+console.log(typeof (stringsapId))
+
+
+let name = "saad"
+
+console.log(name)
+
+console.log(typeof(name))
+
+// string to number => NaN -> Not a Number 
+
+let valueInNumber = Number(name)
+
+console.log(valueInNumber)
+
+console.log(typeof valueInNumber)
