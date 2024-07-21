983c639bdc74056867bc8cdeb18fbd444514db6b
M Saad Asad <saadasadcs@gmail.com>
2024-07-21 08:06:01 +0000

    container config done

---
+++ jsbasics/variables.js
@@ -0,0 +1,14 @@
+const id = 70109951
+
+let name = "Saadiee" // best approach is, use the let keyword for intilize a variable
+
+var email = "saadiee@gmail.com"
+
+city = "Lahore" // by default type of variable consider as var 
+
+// id = 70109453 not allowed as the keyword is const only declared once 
+
+
+console.log(id);
+
+console.table([id,name,email,city]);
