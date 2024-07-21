0c89ce9317f1bd26b7fb4032bbba3555b4ad2caf
M Saad Asad <saadasadcs@gmail.com>
2024-07-21 09:04:47 +0000

    container config done

---
+++ jsbasics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //we use this keyword to tell the compiler to  treat all JS code as newer version but now it is by default behavior 
+
+// alert( 3 + 3) // we are using nodejs, not browser an the alert pop up will appear when it is embeded it html tags open in browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Saad")
+
+
+let name = "Saad"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value / empty
+// undefined => valuse is not defined yet
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
