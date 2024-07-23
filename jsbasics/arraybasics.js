dcc9692ad0d0dd79fb63ea7b4bfffe56b745fb6b
M Saad Asad <saadasadcs@gmail.com>
2024-07-23 17:55:19 +0000

    Array Basic Concepts

---
+++ jsbasics/arraybasics.js
@@ -0,0 +1,53 @@
+const arr = [0, 1, 2, 3, 4, 5]
+
+console.log(arr)
+
+const arry = new Array ("Saad", "Asad", "Ali" , "Umair") // another way of declaration of an array
+
+console.log(arry)
+
+console.log(arry[1])
+
+// Array Methods
+
+arr.push(6) // insert the element into an array at last index
+
+console.log(arr)
+
+arr.pop() // Remove the element from the array at last index
+
+console.log(arr)
+
+arr.unshift(7) // insert the element into an array at the zero index
+
+console.log(arr)
+
+arr.shift() // Remove the element from array at the zero index
+
+console.log(arr)
+
+console.log(arr.includes(9)) // tell whether 9 exits in an array or not i.e true or false
+
+console.log(arr.indexOf(9)) // tell index 
+
+const newArr = arr.join() // Adds all the elements of an array into a string, separated by the specified separator string.
+
+console.log(arr) // return a array typeof => Object
+
+console.log(typeof arr)
+
+console.log(newArr) // return a string 
+
+console.log(typeof newArr)
+
+
+
+
+
+
+
+
+
+
+
+
