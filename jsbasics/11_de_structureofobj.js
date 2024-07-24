b72bc67634aef635e7e2ba4d0985e3a293257463
M Saad Asad <saadasadcs@gmail.com>
2024-07-24 13:26:30 +0000

    Javascript Basics

---
+++ jsbasics/11_de_structureofobj.js
@@ -0,0 +1,14 @@
+const studentRecord = {
+
+    name : "Saad",
+    emai:"saadasad@cs.com",
+    sapid:70109951
+}
+
+// const {sapid} = studentRecord
+
+// console.log(sapid)
+
+const {sapid : id } = studentRecord
+
+console.log(id) 
