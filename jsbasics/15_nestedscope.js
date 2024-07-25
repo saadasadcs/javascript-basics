3bffd747101db21fa849a68e85a68e592718818e
M Saad Asad <saadasadcs@gmail.com>
2024-07-25 12:20:49 +0000

    Javascript Basics

--- jsbasics/15_nestedscope.js
+++ jsbasics/15_nestedscope.js
@@ -1,20 +1,53 @@
-function stName(){
-    const username = "Saad";
+// function stName(){
+//     const username = "Saad";
 
-    console.log(username);
+//     console.log(username);
 
-    function stEmail(){
-        const email ="saadasad@cs.com";
-        console.log("Username :", username + " UserEmail :", email);
-    }
+//     function stEmail(){
+//         const email ="saadasad@cs.com";
+//         console.log("Username :", username + " UserEmail :", email);
+//     }
 
-     // console.log(email) // Variables declared inside a block are not accessible outside that block.
+//      // console.log(email) // Variables declared inside a block are not accessible outside that block.
 
-     stEmail() 
-    }
+//      // stEmail() 
+//     }
 
     
 // console.log(username) // Variables declared inside a block are not accessible outside that block.
     
-stName()
-    
+// stName()
+
+
+
+// Ex : 2
+
+
+// if (true) {
+//     const username = "Saad"
+//     if (username === "Saad") {
+//         const email = " saadasad@cs.com"
+//         console.log(username + email);
+//     }
+//     // console.log(email);
+// }
+
+// // console.log(username);
+
+
+
+
+// Ex : 3
+
+// console.log(addone(5))
+
+// function addone(num){
+//     return num + 1
+// }
+
+// another way to define a function
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
