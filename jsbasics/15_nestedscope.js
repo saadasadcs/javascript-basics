23cfeec215b27daff01998306c84816715b644cc
M Saad Asad <saadasadcs@gmail.com>
2024-07-25 12:05:05 +0000

    Javascript Basics

--- jsbasics/15_nestedscope.js
+++ jsbasics/15_nestedscope.js
@@ -1,22 +1,20 @@
-// Nested Scope
-
 function stName(){
-    const username = "Saad"
+    const username = "Saad";
 
-    console.log(username)
+    console.log(username);
 
     function stEmail(){
-        const email ="saadasad@cs.com"
-        console.log("Username :", username + "UserEmail :", email)
+        const email ="saadasad@cs.com";
+        console.log("Username :", username + " UserEmail :", email);
     }
 
-    // console.log(email) // Variables declared inside a block are not accessible outside that block.
+     // console.log(email) // Variables declared inside a block are not accessible outside that block.
+
+     stEmail() 
+    }
 
     
-}
-
 // console.log(username) // Variables declared inside a block are not accessible outside that block.
-
-// stName()
-
-stEmail() 
+    
+stName()
+    
