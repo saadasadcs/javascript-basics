c894944b519c4212f3cdc2e73d493dd569c9fb6d
M Saad Asad <saadasadcs@gmail.com>
2024-07-30 08:47:46 +0000

    for of

---
+++ jsbasics/23_forof.js
@@ -0,0 +1,21 @@
+/*
+
+The for...of statement executes a loop that operates on a sequence of values sourced from an iterable object. 
+
+*/
+
+const myArry = [7,0,1,0,9,9,5,1,5,0,7]
+
+for (const arr of myArry) {
+
+    // console.log(arr)
+    
+}
+
+
+
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(greet)
+}
